dc5d79adce74217c95cfbce97bcbf55a26c172d1
snyted <joaofarias_16@hotmail.com>
2026-05-20 10:44:27 -0300

    Updates default server port and refines middleware import
    Changes default server port to 5432 for improved environment alignment.
    Refactors middleware imports for clarity and organization.

--- src/server.js
+++ src/server.js
@@ -1,17 +1,19 @@
 import dotenv from "dotenv";
 dotenv.config();
+
 import express from "express";
 import logGlobal from "./middlewares/logGlobal.js";
+import { errorHandler } from "./middlewares/errorHandler.js";
+
 import user from "./routes/userRoutes.js";
 import search from "./routes/searchRoutes.js";
 import movies from "./routes/moviesRoutes.js";
 import series from "./routes/seriesRoutes.js";
 import auth from "./routes/authRoutes.js";
-import { errorHandler } from "./middlewares/errorHandler.js";
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5432;
 
 app.use(express.json());
 
