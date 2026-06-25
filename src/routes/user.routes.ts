010b21effe31fe33fb7652a8fa50ecd158900710
snyted <joaofarias_16@hotmail.com>
2026-06-25 17:26:53 -0300

    refact: reorganize user routes; migrate to TypeScript

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,22 +1,19 @@
 import { Router } from "express";
+// import { authenticate } from "../middlewares/authenticate.middleware.js";
 import { MediaController } from "../controllers/media.controller.js";
-import { MediaService } from "../services/media.service.js";
 import { MediaRepository } from "../repositories/media.repository.js";
 import { TMDBProvider } from "../providers/tmdb/tmdb.provider.js";
-
-export const router = Router();
+import { MediaService } from "../services/media.service.js";
 
 const mediaRepository = new MediaRepository()
 const mediaProvider = new TMDBProvider()
 const mediaService = new MediaService(mediaRepository, mediaProvider)
 const mediaController = new MediaController(mediaService);
 
-router.get("/", mediaController.trending);
-router.get("/search", mediaController.search)
-router.get("/:id", mediaController.findById);
-
-router.post("/:id/favorite", mediaController.toggleFavorite);
-router.put("/:id/rate", mediaController.createRate);
-router.put("/:id/review", mediaController.createReview);
+const router = Router();
+router.get("/");
+router.get("/favorites", mediaController.showFavorites);
+router.get("/rates", mediaController.showRates);
+router.get("/reviews", mediaController.showReviews);
 
 export default router;
