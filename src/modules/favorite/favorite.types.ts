63b69fdf2f8af468c3dd55aad39f925ee679c811
snyted <joaofarias_16@hotmail.com>
2026-06-30 12:33:56 -0300

    refact: consolidate ToggleFavoriteDTO into favorite.types.ts and enhance AppFavorite structure

--- src/modules/favorite/favorite.types.ts
+++ src/modules/favorite/favorite.types.ts
@@ -1,7 +1,24 @@
 import { AppMedia } from "../media/media.types.js";
+import { MediaType } from "@prisma/client";
+
+
+// domain
+export interface IFavoriteRepository {
+    insert(userId: number, mediaId: number): Promise<void>,
+    find(userId: number, mediaId: number): Promise<AppFavorite | null>,
+    delete(userId: number, mediaId: number): Promise<void>,
+    userList(userId: number): Promise<AppFavorite[] | null>,
+};
 
 export interface AppFavorite {
     userId: number,
-    media: AppMedia
-}
+    media: AppMedia,
+};
 
+
+// DTO
+export interface ToggleFavoriteDTO {
+    userId: number,
+    mediaId: number,
+    mediaType: MediaType,
+};
--- src/modules/favorite/toggle-favorite.dto.ts
+++
@@ -1,7 +0,0 @@
-import { MediaType } from "@prisma/client";
-
-export interface ToggleFavoriteDTO {
-    mediaId: number,
-    userId: number,
-    mediaType: MediaType,
-}
