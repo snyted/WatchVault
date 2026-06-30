29bf66ced11146b2c529c5264d5d90d6665654ec
snyted <joaofarias_16@hotmail.com>
2026-06-30 08:48:17 -0300

    refact: reorganize media types and enhance UserMediaResponseDTO structure

--- src/modules/media/media.dto.ts
+++
@@ -1,6 +0,0 @@
-import { Media } from "@prisma/client";
-export interface UserMediaResponseDTO extends Omit<Media, "id" | "createAt" | "updatedAt"> {
-  rate?: number | null;
-  review?: string | null;
-  favorited?: boolean;
-}
--- src/modules/media/media.types.ts
+++ src/modules/media/media.types.ts
@@ -1,5 +1,4 @@
-import { MediaType } from "@prisma/client";
-
+import { Media, MediaType } from "@prisma/client";
 export interface AppMedia {
     tmdbId: number,
     title: string,
@@ -11,11 +10,21 @@ export interface AppMedia {
 }
 
 export interface IMediaRepository {
-    create(data: AppMedia): Promise<void>,
-    find(id: number, type: MediaType): Promise<AppMedia>,
+    insert(data: AppMedia): Promise<AppMedia>,
+    findById(tmdbId: number, type: MediaType): Promise<AppMedia | null>,
+    searchByName(name: string): Promise<AppMedia[] | null>,
 }
 
 export interface MediaRequestQuery {
   type?: string;
   page?: string;
+}
+
+export type AppMediaType = MediaType
+
+
+export interface UserMediaResponseDTO extends Omit<Media, "id" | "createAt" | "updatedAt"> {
+  rate?: number | null;
+  review?: string | null;
+  favorited?: boolean;
 }
