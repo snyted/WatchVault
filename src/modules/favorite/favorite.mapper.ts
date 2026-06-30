0578fbcff0e0350013bdb196a6a011518e2065ce
snyted <joaofarias_16@hotmail.com>
2026-06-30 12:37:10 -0300

    feat: implement FavoriteService and FavoriteController for managing user favorites; add repository methods for insert, find, and delete operations

--- src/modules/favorite/favorite.mapper.ts
+++ src/modules/favorite/favorite.mapper.ts
@@ -1,12 +1,12 @@
 import { AppFavorite } from "./favorite.types.js";
 import { FavoriteWithMediaPrisma } from "./favorite.repository.js";
-import { MediaStorageMapper } from "../media/media.mapper.js";
+import { MediaMapper } from "../media/media.mapper.js";
 
 export class FavoriteMapper {
     static toDomain(data: FavoriteWithMediaPrisma): AppFavorite {
         return {
             userId: data.userId,
-            media: MediaStorageMapper.toDomain(data.media),
+            media: MediaMapper.toDomain(data.media),
         }
     }
 }
--- src/modules/favorite/favorite.repository.ts
+++ src/modules/favorite/favorite.repository.ts
@@ -1,29 +1,64 @@
 import { Prisma } from "@prisma/client";
 import prisma from "../../shared/config/prisma.js";
-import { AppFavorite } from "./favorite.types.js";
+import { AppFavorite, IFavoriteRepository } from "./favorite.types.js";
 import { FavoriteMapper } from "./favorite.mapper.js";
 
 export type FavoriteWithMediaPrisma = Prisma.FavoriteGetPayload<{ include: { media: true } }>
 
-export class FavoriteRepositoy {
-
-    constructor() {
+export class FavoriteRepositoryPrisma implements IFavoriteRepository {
+    public async insert(userId: number, mediaId: number): Promise<void> {
+        try {
+            await prisma.favorite.create({
+                data: {
+                    userId,
+                    mediaId,
+                }
+            })
+        } catch (error) {
+            console.error(error)
+            throw error
+        }
 
     }
+    public async find(userId: number, mediaId: number): Promise<AppFavorite | null> {
+        try {
+            const fav = await prisma.favorite.findUnique({
+                where: {
+                    userId_mediaId: {
+                        userId,
+                        mediaId,
+                    }
+                },
+                include: {
+                    media: true,
+                }
+            })
 
-    public async find(userId: number, mediaId: number): Promise<null> {
-        return null
+            if (!fav) {
+                return null
+            }
+
+            return FavoriteMapper.toDomain(fav);
+        } catch (error) {
+            console.error(error)
+            throw error
+        }
     }
 
     public async delete(userId: number, mediaId: number): Promise<void> {
-
+        await prisma.favorite.delete({
+            where: {
+                userId_mediaId: {
+                    userId,
+                    mediaId,
+                }
+            }
+        })
     }
 
-    public async insert(userId: number, mediaId: number): Promise<any> {
 
-    }
 
-    public async show(userId: number): Promise<AppFavorite[] | null> {
+    public async userList(userId: number): Promise<AppFavorite[] | null> {
         const favs = await prisma.favorite.findMany({
             where: {
                 id: userId,
@@ -39,5 +74,4 @@ export class FavoriteRepositoy {
 
         return favs.map((fav) => FavoriteMapper.toDomain(fav))
     }
-
 }
