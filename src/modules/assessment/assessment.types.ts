35c685e68dcd02d783a295801786ca81c1037720
snyted <joaofarias_16@hotmail.com>
2026-07-02 00:47:57 -0300

    refact: streamline IAssesmentRepository interface and update Review structure; remove unused Rating interface

--- src/modules/assessment/assessment.types.ts
+++ src/modules/assessment/assessment.types.ts
@@ -1,43 +1,29 @@
+import { MediaType } from "@prisma/client";
+
 export interface IAssesmentRepository {
-    insertReview(data: Review): Promise<Review>;
-    insertRating(data: Rating): Promise<Rating>;
-
-    UpdateReview(userId: number, mediaId: number, newReview: string): Promise<Review>;
-
-    deleteReview(userId: number, mediaId: number): Promise<void>;
-    deleteRating(userId: number, mediaId: number): Promise<void>; 
-
-    listReviews(userId: number): Promise<Review[]>;
-    listRatings(userId: number): Promise<Rating[]>;
-}
+    insert(data: Review): Promise<void>,
+    update(userId: number, mediaId: number, newReview: string): Promise<Review>,
+    findById(userId: number, mediaId: number): Promise<boolean>
+    delete(userId: number, mediaId: number): Promise<void>,
+    userReviews(userId: number): Promise<Review[]>,
+    mediaReviews(mediaId: number): Promise<Review[]>,
+};
 
 // Domain
 export interface Review {
     userId: number,
     mediaId: number,
-    review: string,
+    review?: string | null,
     rating: number,
-}
-
-export interface Rating {
-    userId: number,
-    mediaId: number,
-    rating: number,
-}
+};
 
 // Requests
-export interface CreateReviewDTO {
+export interface UpsertReviewDTO {
+    userId: number,
     mediaId: number,
-    review: string,
+    review?: string,
     rating: number,
-}
-
-export interface CreateRatingDTO {
-    mediaId: number,
-    rating: number,
-}
+};
 
 // Response
-export type ReviewResponse = Review
-
-export type RatingResponse = Rating
+export type ReviewResponse = Review;
